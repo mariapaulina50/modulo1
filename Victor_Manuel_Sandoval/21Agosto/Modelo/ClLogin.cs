6555c95034c94c23644cadeab0b039d0bdcb20a5
victor98722 <victor.sandoval98722@gmail.com>
2019-08-21 13:52:18 -0500

    insert OK

---
+++ Victor_Manuel_Sandoval/21Agosto/Modelo/ClLogin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class ClLogin
+    {
+        
+        public static int IniciarSesion(String user, String pass)
+        {
+            EntidadDataContext context = new EntidadDataContext();
+            var query = context.usuarios.Where(p => p.usuario == user && p.clave == pass).Select(p => p);
+            int IDRol = 0;
+            if(query.Count() > 0)
+            {
+                IDRol = query.First().idRol;
+            }
+            return IDRol;
+        }
+
+    }
+}
---
+++ Victor_Manuel_Sandoval/21Agosto/Vista/Login.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Modelo;
+
+namespace Vista
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            string user = txtusuario.Text;
+            string pass = txtclave.Text;
+
+            int Login = ClLogin.IniciarSesion(user, pass);
+
+            switch (Login)
+            {
+                case 1:
+                    Response.Redirect("Menu.aspx");
+                    break;
+                case 2:
+                    Response.Redirect("Menu2.aspx");
+                    break;
+                default:
+                    Response.Write("<script>alert('Usuario no existe')</script>");
+                    break;
+            }
+
+        }
+    }
+}
